a2f8bcdc995553ef9111120e0f281ef6de9c926f
LENY DESLOGES <lenydesloges@MacBook-Air-de-LENY.local>
2025-01-10 14:54:27 +0100

    blabla

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+ <link href="./include/style/style.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+    <div class="titre"><B>Domaine DESLOGES</B></div>
+    <div>vignoble</div>
+    <div>contact</div>
+    <div>vente</div>
+    <div>image</div>
+    <div> href="google.fr"</div>
+    <div>vignoble</div>
+    <div>vignoble</div>
+    <div>vignoble</div>
+    
+</body>
+
+</html>
