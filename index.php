d4c5ab09071dcf5568da84ee1b061cabc9d481ac
LENY DESLOGES <lenydesloges@MacBook-Air-de-LENY.local>
2025-01-12 17:30:55 +0100

    blabbla

--- index.php
+++ index.php
@@ -2,20 +2,116 @@
 <html>
 
 <head>
- <link href="./include/style/style.css" rel="stylesheet" type="text/css">
+ <link href="./include/style/style.css" rel="stylesheet" type="text/css">  
 </head>
 
 <body>
-    <div class="titre"><B>Domaine DESLOGES</B></div>
-    <div>vignoble</div>
-    <div>contact</div>
-    <div>vente</div>
-    <div>image</div>
-    <div> href="google.fr"</div>
-    <div>vignoble</div>
-    <div>vignoble</div>
-    <div>vignoble</div>
+<div class="bandeau"> <B>Domaine Desloges </B></div>
+<a href= "/src/Le_Domaine" class="bouton1"> Le Domaine </a>
+<a href= "/src/Nos_Vins" class="bouton2"> Nos Vins </a>
+<a href= "/src/Contact" class="bouton3"> Contact </a>
+<a href= "/src/Actus" class="bouton4"> Actus </a>
+<a href= "/src/francais" class="bouton5"> 🇫🇷 </a>
+<a href= "/src/anglais" class="bouton6"> 🇬🇧 </a>
+<img src="https://routetouristiquedelavalleeducher.fr/wp-content/uploads/2018/02/desloges.png" alt="Photo en ligne" class="carte">
+
+
+
+<style>
+    .bandeau {
+        background-color: #2F4F4F;
+        padding: 20px ;
+        position: fixed ;
+        color: #FFEFD5 ;
+        width: 100% ;
+        text-align: left ;
+        top: 0 ;
+        left: 0px ;
+        font-size : 27px ;
+        font-family: 'Courier New', Courier, monospace;
+    }
+
+    .bouton1 {
+        position: absolute;
+        color :#FFEFD5;
+        top : 15px ;
+        left : 790px ;
+        padding : 10px ;
+        z-index:1000;
+        font-size: 19px;
+        font-family: 'Courier New', Courier, monospace;
+        text-decoration: NONE;
+    }
+
+    .bouton2 {
+        position: absolute;
+        color :#FFEFD5;
+        top : 15px ;
+        left : 970px ;
+        padding : 10px ;
+        z-index:1075;
+        font-size: 19px;
+        font-family: 'Courier New', Courier, monospace;
+        text-decoration: NONE;
+    }
+
+    .bouton3 {
+        position: absolute;
+        color :#FFEFD5;
+        top : 15px ;
+        left : 1120px ;
+        padding : 10px ;
+        z-index:1175;
+        font-size: 19px;
+        font-family: 'Courier New', Courier, monospace;
+        text-decoration: NONE;
+    }
+
+    .bouton4 {
+        position: absolute;
+        color :#FFEFD5;
+        top : 15px ;
+        left : 1250px ;
+        padding : 10px ;
+        z-index:1000;
+        font-size: 19px;
+        font-family: 'Courier New', Courier, monospace;
+        text-decoration: NONE;
+    }
+
+    .bouton5 {
+        position: absolute;
+        top : 18px;
+        left :1349px;
+        font-size: 21px;
+        text-decoration: NONE;
+    }
+
+    .bouton6 {
+        position: absolute;
+        top : 18px;
+        left :1400px;
+        font-size: 21px;
+        text-decoration: NONE;
+    }
+
+    .carte {
+        position: absolute;
+        top : 50px;
+        left :0px;
+        font-size: 21px;
+        text-decoration: NONE;
+        width:300px;
+        height: auto;
+
+    }
+
+
+
+
     
+
+</style>    
 </body>
 
 </html>
